bbf90ebbe2a2e5884c0d6714cb49329cb51ed244
Myeruyert <myeruyertjanibyek@gmail.com>
2024-06-30 11:28:34 +0800

    created the design with bootstrap

---
+++ main.js
@@ -0,0 +1,10 @@
+outsideBox = document.getElementsByClassName("containerBox")[0];
+console.log("outsideBox", outsideBox);
+
+taskBox = document.getElementsByClassName("box")[0];
+console.log("taskBox", taskBox);
+
+
+    // taskBox = document.createElement("div");
+    // taskBox.className = "box col bg-black border border-secondary rounded p-sm-3";
+    // outsideBox.appendChild(taskBox);
