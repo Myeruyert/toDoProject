420e71f556bfca8e1e1a5f01e9bc384503e2b98f
Myeruyert <myeruyertjanibyek@gmail.com>
2024-07-03 12:33:11 +0800

    done

--- app.js
+++ app.js
@@ -7,18 +7,20 @@ const addTaskBtn = document.getElementById("addTaskBtn");
 const saveBtn = document.getElementById("save-btn");
 const taskInput = document.getElementById("task-input");
 const taskStatus = document.getElementById("status");
+const tooluurTodo = document.getElementsByClassName("tooluur")[0];
+const tooluurIp = document.getElementsByClassName("tooluur-2")[0];
+const tooluurDone = document.getElementsByClassName("tooluur-3")[0];
+const tooluurBlocked = document.getElementsByClassName("tooluur-4")[0];
 
 // VARIABLES FOR TASK
+let isEdited = false;
+let editedIndex = -1;
+
 const tasks = [
-  {
-    name: "Task Two",
-    status: "INPROGRESS",
-    border: "YELLOW",
-  },
-  {
-    name: "Task Three",
-    status: "BLOCKED",
-  },
+  // {
+  //   name: "Task One",
+  //   status: "TODO",
+  // },
 ];
 
 function zurah() {
@@ -26,43 +28,52 @@ function zurah() {
   taskProgressList.innerHTML = "";
   taskDoneList.innerHTML = "";
   taskBlockedList.innerHTML = "";
+  let countTodo = 0;
+  let countIP = 0;
+  let countDone = 0;
+  let countBlocked = 0;
 
   for (let i = 0; i < tasks.length; i++) {
     console.log("i", i);
     console.log("TASKS", tasks);
     const newTaskCard = `
-    <div class="d-flex justify-content-between align-items-center border border-1 rounded p-2 ${statusColor(
+    <div class="d-flex justify-content-between align-items-center border border-1 bg-dark text-white-50 my-2 rounded p-2 ${statusColor(
       tasks[i].status
     )}"}>
-    <span>${tasks[i].name} - ${i}</span>
-    <div>
-        <button class="btn" onclick={editTask()}>
-        <i class="bi bi-pencil"></i>
+      <span>${tasks[i].name} - ${i}</span>
+      <div>
+        <button class="btn"           
+          data-bs-toggle="modal"
+          data-bs-target="#taskModal" 
+          onclick={editTask(${i})}>
+          <i class="bi bi-pencil text-white"></i>
         </button>
         <button class="btn" onclick={deleteTask(${i})}>
-        <i class="bi bi-trash"></i>
+        <i class="bi bi-trash text-danger"></i>
         </button>
-    </div>
+      </div>
     </div>
  `;
-    console.log("n0", newTaskCard);
 
     switch (tasks[i].status) {
       case "TODO": {
         taskTodoList.innerHTML += newTaskCard;
-
+        countTodo++;
         break;
       }
       case "INPROGRESS": {
         taskProgressList.innerHTML += newTaskCard;
+        countIP++;
         break;
       }
       case "DONE": {
         taskDoneList.innerHTML += newTaskCard;
+        countDone++;
         break;
       }
       case "BLOCKED": {
         taskBlockedList.innerHTML += newTaskCard;
+        countBlocked++;
         break;
       }
       default: {
@@ -73,7 +84,7 @@ function zurah() {
     function statusColor(status) {
       switch (status) {
         case "TODO": {
-          return "border-black";
+          return "border-white";
         }
         case "INPROGRESS": {
           return "border-warning";
@@ -90,22 +101,39 @@ function zurah() {
       }
     }
   }
+  tooluurTodo.innerHTML = countTodo;
+  tooluurIp.textContent = countIP;
+  tooluurDone.textContent = countDone;
+  tooluurBlocked.textContent = countBlocked;
 }
-
-saveBtn.addEventListener("click", function () {
-  const newTask = {
-    name: taskInput.value,
-    status: taskStatus.value,
-  };
-  tasks.push(newTask);
-  zurah();
-  console.log("TASKS", tasks);
-});
-
 zurah();
 
-function editTask() {
-  console.log("edit");
+saveBtn.addEventListener("click", function () {
+  if (isEdited) {
+    tasks[editedIndex].name = taskInput.value;
+    tasks[editedIndex].status = taskStatus.value;
+    isEdited = false;
+  } else {
+    const newTask = {
+      name: taskInput.value,
+      status: taskStatus.value,
+    };
+    tasks.push(newTask);
+
+    zurah();
+    console.log("TASKS", tasks);
+  }
+  taskInput.value = "";
+  taskStatus.value = "TODO";
+  zurah();
+});
+
+function editTask(taskIndex) {
+  console.log("edit", taskIndex);
+  taskInput.value = tasks[taskIndex].name;
+  taskStatus.value = tasks[taskIndex].status;
+  isEdited = true;
+  editedIndex = taskIndex;
 }
 
 function deleteTask(taskIndex) {
